2c80ec6f1e36df996740318f8ec7b3e314614ee5
me <me@me.local>
2023-12-02 00:18:56 -0800

    new events

--- pts.py
+++ pts.py
@@ -17,12 +17,22 @@ class Player:
     self.index = index
     self.spawnpoint = spawnpoint
 
-unit_id = 0
-class Unit:
+sid = 0
+class Idd:
+  def __init__(self):
+    global sid
+    self.id = sid
+    sid += 1
+
+  def __hash__(self):
+    return hash(self.id)
+
+  def __eq__(self, other):
+    return hash(self.id)
+
+class Unit(Idd):
   def __init__(self, player):
-    global unit_id
-    self.id = unit_id
-    unit_id += 1
+    Idd.__init__(self)
     self.player = player
     self.personality = random.randint(1, 255)
     self.skin = (random.randint(1, 255),
@@ -32,10 +42,11 @@ class Unit:
   def __str__(self):
     return "[unit player={}]".format(self.player.index)
 
-class Event:
+class Event(Idd):
   # if old_pos is None, the unit is spawning
   # if new_pos is None, the unit is dying
   def __init__(self, tick, unit, old_pos, new_pos):
+    Idd.__init__(self)
     self.tick = tick
     self.unit = unit
     self.old_pos = old_pos
@@ -51,22 +62,22 @@ class Client:
     self.player_index = player_index
     self.map_w = map_w
     self.map_h = map_h
-    self.map = {}
     self.dead = set()
-    self.dead_units = set()
-    for x in range(self.map_w):
-      for y in range(self.map_h):
-        self.map[(x, y)] = None
+    self.perma_dead = set()
+    self.units = {}  # unit => position
+    self.seen_events = set()  # IDs
     self.initialized = True
 
   def handle_event(self, event):
+    if event.id in self.seen_events:
+      return
+    self.seen_events.add(event.id)
     if event.old_pos and not event.new_pos:
       self.dead.add(event.old_pos)
-      self.dead_units.add(event.unit.id)
-    if event.old_pos:
-      self.map[event.old_pos] = None
-    if event.new_pos and event.unit.id not in self.dead_units:
-      self.map[event.new_pos] = event.unit
+      self.perma_dead.add(event.unit)
+      del self.units[event.unit]
+    if event.new_pos and event.unit not in self.perma_dead:
+      self.units[event.unit] = event.new_pos
 
   def tick(self):
     self.dead.clear()
@@ -84,17 +95,16 @@ class Server:
     if self.initialized:
       return
     self.clients = clients
-    self.comm_speed = 1 / 5
+    self.event_speed = 5
     self.tick_no = 0
     self.map_w = map_w
     self.map_h = map_h
     self.map = {}
-    self.event_centers = []  # (event, center point, radius)
-    self.event_map = {}
+    self.event_centers = {}  # event => (center point, radius)
+    self.event_map = collections.defaultdict(set)  # point => set of events
     for x in range(map_w):
       for y in range(map_h):
         self.map[(x, y)] = None
-        self.event_map[(x, y)] = []
     self.new_map = dict(self.map)
     self.players = []
     center = (map_w / 2, map_h / 2)
@@ -107,14 +117,47 @@ class Server:
     self.initialized = True
 
   def broadcast_event(self, event, pos):
-    self.event_centers.append((event, pos, 0))
+    self.event_centers[event] = (pos, 0)
 
   def expand_events(self):
     def circle(center, radius):
-      # TODO TODO
-      return []
-    for event, center, radius in self.event_centers:
-      old = circle(center, radius)
+      # (x-a)^2 + (y-b)^2 = r^2
+      # => y = b +-sqrt(r^2 - (x-a)^2)
+      a = center[0]
+      b = center[1]
+      for x in range(a - radius, a + radius + 1):
+        s = math.sqrt(radius ** 2 - (x - a) ** 2)
+        for y in [b + s, b - s]:
+          y = int(round(y))
+          yield (x, y)
+          # also round inwards
+          # TODO this is jank
+          x2 = x - 1 if x > 0 else x + 1
+          y2 = y - 1 if y > 0 else y + 1
+          yield (x2, y)
+          yield (x, y2)
+          yield (x2, y2)
+    to_delete = []
+    to_set = []
+    for event, el in self.event_centers.items():
+      center, radius = el
+      for x, y in circle(center, radius):
+        if event in self.event_map[(x, y)]:
+          self.event_map[(x, y)].remove(event)
+      inside = False
+      for x, y in circle(center, radius + 1):
+        if x >= 0 and x < self.map_w and y >= 0 and y < self.map_h:
+          inside = True
+          self.event_map[(x, y)].add(event)
+      if inside:
+        to_set.append((event, center, radius + 1))
+      else:
+        to_delete.append(event)
+    for event, center, radius in to_set:
+      self.event_centers[event] = (center, radius)
+    for event in to_delete:
+      del self.event_centers[event]
+
 
   def spawn(self, unit, pos):
     if not self.map[pos]:
@@ -133,14 +176,15 @@ class Server:
     if occupied:
       if occupied.player.index != unit.player.index:
         # collide, killing both units
+        #debug("collision at", new_x, new_y)
         self.new_map[(x, y)] = None
         self.new_map[(new_x, new_y)] = None
         self.broadcast_event(Event(self.tick_no, unit,
-          old_pos=(x, y), new_pos=(new_x, new_y)), (x, y))
+          old_pos=(x, y), new_pos=(new_x, new_y)), (new_x, new_y))
         self.broadcast_event(Event(self.tick_no, unit,
-          old_pos=(new_x, new_y), new_pos=None), (x, y))
+          old_pos=(new_x, new_y), new_pos=None), (new_x, new_y))
         self.broadcast_event(Event(self.tick_no, occupied,
-          old_pos=(new_x, new_y), new_pos=None), (x, y))
+          old_pos=(new_x, new_y), new_pos=None), (new_x, new_y))
         return
       if detour:
         # it's one of our own, just try once to move around it
@@ -162,7 +206,7 @@ class Server:
 
   def tick(self):
     move_speed = 10
-    spawn_rate = 100
+    spawn_rate = 30
     if self.tick_no % spawn_rate == 0:
       self.spawn_phase()
     def move(x, y):
@@ -182,7 +226,8 @@ class Server:
     if self.tick_no % move_speed == 0:
       self.for_all_squares(move)
     self.execute_moves()
-    self.expand_events()
+    if self.tick_no % self.event_speed == 0:
+      self.expand_events()
     for client in self.clients:
       for event in self.event_map[self.players[client.player_index].spawnpoint]:
         client.handle_event(event)
@@ -227,11 +272,12 @@ while running:
   server.setup(2, square_edge, square_edge, [client])
   if frame % (fps // sim_speed) == 0:
     server.tick()
-    client.tick()
     last_tick = frame
     for dead in client.dead:
+    #  debug("explosion at", dead, ", frame", frame)
       explosions[dead] = frame
       sound.play()
+    client.tick()
 
   to_clear = set()
   for k, v in explosions.items():
@@ -266,39 +312,41 @@ while running:
           color = "black"
         pygame.draw.rect(screen, color, (x * scale, y * scale, scale, scale))
 
-  for x in range(server.map_w):
-    for y in range(server.map_h):
-      distance = math.sqrt((x - focal[0]) ** 2 + (y - focal[1]) ** 2)
-      # draw object if needed
-      obj = client.map[(x, y)]
-      if obj:
-        # something is there
-        move_x = 0
-        move_y = 0
-#        if (x, y) in server.moves:
-#          # animate
-#          old_pos = server.moves[(x, y)]
-#          dx = x - old_pos[0]
-#          dy = y - old_pos[1]
-#          mag = (frame - last_tick) / (fps // sim_speed) - 1.0
-#          move_x = dx * mag * scale
-#          move_y = dy * mag * scale
-#          if False and server.map[(x, y)].id % 20 == 0:
-#            debug("animate",
-#                {"frame": frame, "last_tick": last_tick,
-#                  "pos": (x, y), "diff": (dx, dy), "mag": mag,
-#                  "move": (move_x, move_y)})
-        hue = 360 * obj.player.index / len(server.players) + (obj.skin[0] / 12)
-        hue = int(hue) % 360
-        color = pygame.Color(0)
-        color.hsva = (int(hue), 95,
-            int(max(0, 100 - (obj.skin[1] // 8) - distance * 2)),
-            max(0, 100 - (obj.skin[2] // 8)))
-        pygame.draw.rect(screen, color, (
-          int(x * scale + margin + move_x),
-          int(y * scale + margin + move_y),
-          scale - margin * 2,
-          scale - margin * 2), 0, int(scale / 6))
+      if False:  # edit to show events
+        for event in server.event_map[(x, y)]:
+          if event.new_pos: continue   # explosions only
+          color = (0, 150 - 3 * (event.id % 20), 0)
+          pygame.draw.rect(screen, color, (x * scale, y * scale, scale, scale))
+
+  for unit, pos in client.units.items():
+    x, y = pos
+    distance = math.sqrt((x - focal[0]) ** 2 + (y - focal[1]) ** 2)
+    move_x = 0
+    move_y = 0
+#    if (x, y) in server.moves:
+#      # animate
+#      old_pos = server.moves[(x, y)]
+#      dx = x - old_pos[0]
+#      dy = y - old_pos[1]
+#      mag = (frame - last_tick) / (fps // sim_speed) - 1.0
+#      move_x = dx * mag * scale
+#      move_y = dy * mag * scale
+#      if False and server.map[(x, y)].id % 20 == 0:
+#        debug("animate",
+#            {"frame": frame, "last_tick": last_tick,
+#              "pos": (x, y), "diff": (dx, dy), "mag": mag,
+#              "move": (move_x, move_y)})
+    hue = 360 * unit.player.index / len(server.players) + (unit.skin[0] / 12)
+    hue = int(hue) % 360
+    color = pygame.Color(0)
+    color.hsva = (int(hue), 95,
+        int(max(50, 100 - (unit.skin[1] // 8) - distance * 2)),
+        max(50, 100 - (unit.skin[2] // 8)))
+    pygame.draw.rect(screen, color, (
+      int(x * scale + margin + move_x),
+      int(y * scale + margin + move_y),
+      scale - margin * 2,
+      scale - margin * 2), 0, int(scale / 6))
 
   # print debug
   for i, line in enumerate(terminal_lines):
